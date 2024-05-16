706e1c03783bbd5e77b6790ea429c6f4e47a6de6
Cristhian Vasquez <cristhianvasquez99@gmail.com>
2024-05-16 16:17:27 -0500

    se crea boton de mercado pago

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-const saludo = "holis"
+const pago = document.querySelector("#btn-pago")
+
+async function mercadoPago(){
+    const llamado = await fetch("https://api.mercadopago.com/v1/payment_methods")
+    console.log(llamado)
+}
+
+pago.addEventListener("click", mercadoPago)
